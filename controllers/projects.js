08a1b03c590fbd1a6777eb5b8d02525a480e54e4
hnorthcote <hnorthcote2000@hotmail.com>
2020-06-19 13:17:42 -0600

    Final touches
    missing footer, and deploy

--- controllers/projects.js
+++ controllers/projects.js
@@ -62,3 +62,11 @@ req.body.environment = environment._id
   }
     console.log('created project!');res.redirect('/projects/new');})
   }
+
+
+  function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
