dfb1081c912fd76ecf1b5ff26877c4982c581e1d
hnorthcote <hnorthcote2000@hotmail.com>
2020-06-19 18:06:08 -0600

    finetunning

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const app = express();
 require('./config/database');
 require('./config/passport');
 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(methodOverride('_method'));
